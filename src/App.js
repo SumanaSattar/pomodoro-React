86f48e7b53cee17d16922d1cea2764ab8771b094
Sumana Sattar <50830293+SumanaSattar@users.noreply.github.com>
2021-04-12 15:11:29 +0200

    Timer added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import SetPomodoro from './components/set-pomodoro.component';
 
 function App() {
   return (
     <div className='App'>
       <h1>Pomodoro</h1>
       <p>Focus on being productive instead of busy</p>
+      <SetPomodoro/>
     </div>
   );
 }
--- src/components/set-pomodoro.component.jsx
+++ src/components/set-pomodoro.component.jsx
@@ -1,7 +1,44 @@
+import Button from './button.component'
+import {useState} from 'react'
 const SetPomodoro = () => {
+    const [newTimer , setNewTimer] = useState({
+        work:0.3,
+        short:0.2,
+        long:1,
+        active:'work'
+    })
+    const handleChange=(e)=>{
+        const {name,value} = e.target;
+        switch (name) {
+            case 'work':
+                setNewTimer({...newTimer,work:parseInt(value)});
+                break;
+            case 'shortBreak':
+                setNewTimer({...newTimer,short:parseInt(value)});
+                break;
+            case 'longBreak':
+                setNewTimer({...newTimer,long:parseInt(value)});
+                break;
+            default:
+                break;
+        }
+        console.log(newTimer)
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(newTimer)
+
+    }
     return ( 
-        <div>
-            
+        <div className="form-container">
+            <form noValidate>
+                <div className="input-wrapper"> 
+                    <input className="input" name="work" onChange={handleChange} value={newTimer.work}/>
+                    <input className="input" name="shortBreak" onChange={handleChange} value={newTimer.short}/>
+                    <input className="input" name="longBreak" onChange={handleChange} value={newTimer.long}/>
+                </div>
+                 <Button title="Set Timer" clickHandle={handleSubmit}/>
+            </form>
         </div>
      );
 }
