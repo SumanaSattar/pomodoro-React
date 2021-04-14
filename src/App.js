c13095cbf9f900b90bc7daae788c94e1489008cc
Sumana Sattar <50830293+SumanaSattar@users.noreply.github.com>
2021-04-14 17:27:30 +0200

    setting-context functionality connected

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import './App.css';
+import Button from './components/button.component';
 import CountDown from './components/countdown-animation.component';
 import SetPomodoro from './components/set-pomodoro.component';
 import { SettingContext } from './context/setting-context';
 
 
 function App() {
-  const {pomodoro} = useContext(SettingContext)
+  const {pomodoro,executing,setCurrentTimer,settingBtn,children,startAnimate,startTimer,pauseTimer,updateExecute} = useContext(SettingContext)
+  useEffect(()=> updateExecute(executing) , [executing , startAnimate])
   return (
     <div className='App'>
       <h1>Pomodoro</h1>
@@ -14,11 +16,56 @@ function App() {
       { pomodoro===0 ?
           <SetPomodoro/> :
        <>
-          <h3>Testing</h3>
-       </>
+          <ul className="labels">
+            <li>
+              <Button 
+                 title='work'
+                 activeClass={executing.active==='work' && 'activeLabel'}
+                 clickHandle={()=>setCurrentTimer('work')}
+              />
+            </li>
+          
+
+        
+            <li>
+              <Button 
+                 title='Short Break'
+                 activeClass={executing.active==='short' && 'activeLabel'}
+                 clickHandle={()=>setCurrentTimer('short')}
+              />
+            </li>
+       
+
+       
+            <li>
+              <Button 
+                 title='Long Break'
+                 activeClass={executing.active==='long' && 'activeLabel'}
+                 clickHandle={()=>setCurrentTimer('long')}
+              />
+            </li>
+        
+          </ul>
+
+          <Button title='Setting' clickHandle={settingBtn}/>
+
+          <div className='time-container'>
+            <div className='time-wrapper'>
+                <CountDown key={pomodoro} timer={pomodoro} animate={startAnimate}>
+                   {children}
+               </CountDown>
+            </div>
+          </div>
+
+          <div className='button-swrapper'>
+            <Button title='Start' activeClass={!startAnimate && 'active'} clickHandle={startTimer}/>
+            <Button title='Pause' activeClass={startAnimate && 'active'} clickHandle={pauseTimer}/>
+          </div>
+         
+        </>
        }
        
-        {/*<CountDown />*/}
+    
 
     </div>
   );
--- src/context/setting-context.js
+++ src/context/setting-context.js
@@ -53,9 +53,9 @@ const SettingContextProvider = (props) => {
         }
     }
 
-    const children = ({remainingTimer}) => {
-        const minutes = Math.floor(remainingTimer/60)
-        const seconds = remainingTimer % 60
+    const children = ({remainingTime}) => {
+        const minutes = Math.floor(remainingTime/60)
+        const seconds = remainingTime % 60
 
         return `${minutes}:${seconds}`
     }
