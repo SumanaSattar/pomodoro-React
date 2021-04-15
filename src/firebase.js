c68ae83b2942e3326fb74906b91c00d8139e2382
Sumana Sattar <50830293+SumanaSattar@users.noreply.github.com>
2021-04-15 20:37:01 +0200

    adding firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore';
+const firebaseConfig = firebase.initializeApp({
+    apiKey:'',
+    authDomain:'',
+    databaseURL:'',
+    projectId:'',
+    storageBucket:'',
+    messageSenderId:'',
+    apiId:''
+});
+export {firebaseConfig as firebase}
